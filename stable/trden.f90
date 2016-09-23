8b511606d1eba481cc857a07e72108a631d8394e
Daniele Padula <mssmsg@flaubert.csc.warwick.ac.uk>
2016-09-23 15:34:54 +0100

    Corrected formatting in the .f90 file

--- stable/trden.f90
+++ stable/trden.f90
@@ -6,15 +6,15 @@ Subroutine gengrid(origin,dvx,dvy,dvz,nx,ny,nz,grid)
 	REAL(8), intent(in) :: dvx,dvy,dvz
 	REAL(8), DIMENSION(3), intent(in) :: origin
 ! Internal and output Variables
-	INTEGER :: ix,iy,iz,nthrd,OMP_GET_NUM_THREADS
+    INTEGER :: ix,iy,iz,nthrd,OMP_GET_NUM_THREADS
     REAL(8), DIMENSION(nx,ny,nz,3), intent(out) :: grid
 
 do iz=1,nz
-	do iy=1,ny
-		do ix=1,nx
-	        grid(ix,iy,iz,1)=origin(1)+(ix-1)*dvx
-		    grid(ix,iy,iz,2)=origin(2)+(iy-1)*dvy
-			grid(ix,iy,iz,3)=origin(3)+(iz-1)*dvz
+    do iy=1,ny
+        do ix=1,nx
+            grid(ix,iy,iz,1)=origin(1)+(ix-1)*dvx
+            grid(ix,iy,iz,2)=origin(2)+(iy-1)*dvy
+            grid(ix,iy,iz,3)=origin(3)+(iz-1)*dvz
         end do
     end do
 end do
@@ -31,7 +31,7 @@ Subroutine diptrde(trden,grid,dv,n,mu)
 	REAL(8), DIMENSION(n), intent(in) :: trden
 	REAL(8), DIMENSION(n,3), intent(in) :: grid
 ! Internal and output Variables
-	INTEGER :: i,OMP_GET_NUM_THREADS
+    INTEGER :: i,OMP_GET_NUM_THREADS
     REAL(8) :: mux,muy,muz
     REAL(8), DIMENSION(3), intent(out) :: mu
 
@@ -65,7 +65,7 @@ Subroutine couptrde(trdena,grida,dva,trdend,gridd,dvd,thresh,na,nd,couptd)
 ! Internal and output Variables
     REAL(8), DIMENSION(3) :: r12
     REAL(8) :: autown,r,couptd_local,width
-	INTEGER :: i,j,OMP_GET_NUM_THREADS
+    INTEGER :: i,j,OMP_GET_NUM_THREADS
     REAL(8), intent(out) :: couptd
 
 ! Description of the Variables:
@@ -74,10 +74,11 @@ Subroutine couptrde(trdena,grida,dva,trdend,gridd,dvd,thresh,na,nd,couptd)
 
 couptd = 0.
 autown = 2.194746d5
-
 width = 1. / dsqrt (2*(dva**(2.0/3.0) + dvd**(2.0/3.0)) )
+
 !$omp parallel private(i,j,r12,r,couptd_local)
 couptd_local = 0.
+
 !$omp do
 do i=1,na
     if (abs(trdena(i)) < thresh) cycle
@@ -92,10 +93,13 @@ do i=1,na
     end do
 end do
 !$omp end do
+
 !$omp critical
 couptd=couptd+couptd_local
 !$omp end critical
+
 !$omp end parallel
+
 couptd=couptd*dva*dvd*autown
 
 end Subroutine couptrde
